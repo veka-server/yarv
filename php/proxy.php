e587d876ae7770934157813198e3fdb3163301f4
Veka <veka61@laposte.net>
2013-12-26 16:56:14 +0100

    modif structurelle

---
+++ php/proxy.php
@@ -0,0 +1,20 @@
+<?php 
+// proxy php pour recuperer les flux
+if(isset($_GET['flux']))
+{
+	// Set your return content type
+	header('Content-type: application/xml');
+
+	// Get that website's content
+	$handle = fopen($_GET['flux'], "r");
+
+	// If there is something, read and return
+	if ($handle) {
+	    while (!feof($handle)) {
+	        $buffer = fgets($handle, 4096);
+	        echo $buffer;
+	    }
+	    fclose($handle);
+	}
+	die();
+}
