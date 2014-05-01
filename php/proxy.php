25203524305f8f05b455d05737b9a366d457e2aa
Veka <veka61@laposte.net>
2014-05-01 14:10:07 +0200

    Update proxy.php
    ajout d'une securité pour ne pas retourner autre chose qu'un flus rss/atom

--- php/proxy.php
+++ php/proxy.php
@@ -6,20 +6,25 @@ $flux = json_decode(file_get_contents('config.json'), true);
 // proxy php pour recuperer les flux
 if(isset($_GET['flux']) && in_array($_GET['flux'], $flux))
 {
-
-	// Set your return content type
-	header('Content-type: application/xml');
-
 	// Get that website's content
 	$handle = fopen($_GET['flux'], "r");
 
 	// If there is something, read and return
 	if ($handle) {
+		$page = '';
 	    while (!feof($handle)) {
 	        $buffer = fgets($handle, 4096);
-	        echo str_replace('&', '&amp;', $buffer);
+	        $page .= str_replace('&', '&amp;', $buffer);
 	    }
 	    fclose($handle);
 	}
+
+	if(isset($page) && ( stripos($page, '<rss') !== false || stripos($page, '<feed') !== false ) )
+	{
+		// Set your return content type
+		header('Content-type: application/xml');
+		echo  $page;		
+	}
+
 	die();
 }
